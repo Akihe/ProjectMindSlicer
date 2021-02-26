927e01bceff44e081feee8572403d6af00318d24
Akihe <77743583+Akihe@users.noreply.github.com>
2021-02-26 12:39:08 +0200

    Update Main.java

--- project/core/src/com/mygdx/game/Main.java
+++ project/core/src/com/mygdx/game/Main.java
@@ -20,9 +20,11 @@ public class Main extends ApplicationAdapter {
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		Gdx.app.log("testi", "testi");
 		Gdx.app.log("toinen","testi");
 		//kolmas testi
+
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
